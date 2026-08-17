29f2003af62d79846eae910983ca25431d030f8c
Rana <2004ranadey@gmail.com>
2026-08-18 02:50:05 +0530

    revisited HTTP servers

---
+++ Week 4 - NodeJS and HTTP/4.2 - HTTP Servers/revisit/index.js
@@ -0,0 +1,14 @@
+// const http = require('http')
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req.baseUrl);
+    res.send("<h1>Hello</h1>")
+})
+
+
+
+app.listen(3000, ()=>console.log("runnign"))
