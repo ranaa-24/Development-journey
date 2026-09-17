38381f7928781a3c68ad28e0bf231b254fbb29a7
Rana <2004ranadey@gmail.com>
2026-09-17 23:11:44 +0530

    tokens auth

---
+++ Week 5 - Headers, Axios, CORS, Image handling/5.2 - Middlewares_part2 and CORS/revist/cors/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors());
+
+
+app.use("/FE", express.static(path.join(__dirname, 'public')));
+
+app.post('/sum', (req, res) => {
+    const { a, b } = req.body;
+    const total = Number(a) + Number(b);
+    console.log(req.body);
+
+    return res.send({ message: `sum is ${total}` });
+});
+
+app.listen(3000, () => console.log('http://127.0.0.1:3000/'));
